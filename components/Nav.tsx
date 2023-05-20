66517deb841fb4cadb30df488c3b7ac0d1f1ab04
Khant Sithu <khantseithuu@gmail.com>
2023-05-20 02:34:02 +0000

    build navbar

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -6,9 +6,11 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
+  const isAunthenticated = true;
+  const [toggleDropDown, setToggleDropDown] = useState(false);
   return (
     <nav className="flex-between w-full mb-16 pt-3">
-      <Link href="/">
+      <Link href="/" className="flex gap-3">
         <Image
           src="/assets/images/logo.svg"
           alt="logo"
@@ -21,29 +23,92 @@ const Nav = () => {
 
       {/* Desktop Nav */}
       <div className="sm:flex hidden">
-        <div className="flex gap-3 md:gap-5">
-          <Link href="/create-prompt" className="black_btn">
-            Create Prompt
-          </Link>
+        {isAunthenticated ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              Create Prompt
+            </Link>
 
+            <button
+              type="button"
+              onClick={() => alert("signout")}
+              className="outline-btn"
+            >
+              Sign Out
+            </button>
+
+            <Link href="/profile">
+              <Image
+                src="/assets/images/logo.svg"
+                alt="profile"
+                width={30}
+                height={30}
+                className="object-contain"
+              />
+            </Link>
+          </div>
+        ) : (
           <button
+            className="outline_btn"
             type="button"
-            onClick={() => alert("signout")}
-            className="outline-btn"
+            onClick={() => console.log("Sign In")}
           >
-            Sign Out
+            Sign In{" "}
           </button>
+        )}
+      </div>
 
-          <Link href="/profile">
+      {/*  Mobile Nav */}
+      <div className="sm:hidden flex relative">
+        {isAunthenticated ? (
+          <div className="flex">
             <Image
               src="/assets/images/logo.svg"
               alt="profile"
               width={30}
               height={30}
               className="object-contain"
+              onClick={() => setToggleDropDown(!toggleDropDown)}
             />
-          </Link>
-        </div>
+
+            {toggleDropDown && (
+              <div className="dropdown">
+                <Link
+                  href={"/profile"}
+                  className="dropdown_link"
+                  onClick={() => setToggleDropDown(false)}
+                >
+                  My Profile
+                </Link>
+                <Link
+                  href={"/create-prompt"}
+                  className="dropdown_link"
+                  onClick={() => setToggleDropDown(false)}
+                >
+                  Create Prompt
+                </Link>
+                <button
+                  type="button"
+                  onClick={() => {
+                    setToggleDropDown(false);
+                    signOut();
+                  }}
+                  className="mt-5 w-full black_btn"
+                >
+                  Sign Out
+                </button>
+              </div>
+            )}
+          </div>
+        ) : (
+          <button
+            className="outline_btn"
+            type="button"
+            onClick={() => console.log("Sign In")}
+          >
+            Sign In{" "}
+          </button>
+        )}
       </div>
     </nav>
   );
